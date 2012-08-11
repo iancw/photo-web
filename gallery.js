ab7108ae3a567a6ee689af96efd7c29c2b2c0636
Ian Will <icwill@gmail.com>
2012-08-11 18:19:07 -0400

    Initial proof of concept

---
+++ gallery.js
@@ -0,0 +1,29 @@
+
+var currentImg=0;
+var allImages=18;
+
+var images=[];
+
+$(window).load(loadImages);
+
+function loadImages()
+{
+	for(i=0; i<allImages; i++)
+	{
+		var img=new Image();
+		img.src='1024/ianwill'+(i+1)+'.jpg';
+		images[images.length] = img;
+	}
+}
+
+function fadeOut(){
+	$('#mainImage').fadeTo(800, 0.0, fadeIn)
+}
+
+function fadeIn(){
+	currentImg = (currentImg + 1) % images.length;
+	var actImg = images[currentImg];
+	$('#mainImage').css({'background-image':"url('" +actImg.src + "')"});
+	$('#mainImage').attr({ width: actImg.width, height: actImg.height});
+	$('#mainImage').fadeTo(800, 1.0, function(){ setTimeout(fadeOut, 5000); });
+}
